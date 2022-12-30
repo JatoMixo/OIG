5ef238ad4e73a296ed07273a9397b90bc1cb5146
JatoMixo <jatomixo@outlook.es>
2022-12-30 12:07:00 +0100

    main

--- LowLevel/cin.cpp
+++ LowLevel/cin.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <map>
+#include <utility>
 
 using std::string, std::cin, std::cout;
 
@@ -14,9 +16,19 @@ int main(){
 
   int nota;
   string alumno;
+  std::map<string, int> alumnos_notas;
 
   while (cin >> alumno >> nota && alumno != "q" && nota != -1) {
     cout << "El gilipollas de " << alumno << " ha sacado un " << nota << '\n';
+    alumnos_notas[alumno] = nota;
+  }
+
+  std::cout << "----------------------------\n";
+
+  for (std::pair<string, int> i : alumnos_notas){
+    std::cout << "Alumno: " << i.first << '\n';
+    std::cout << "Nota: " << i.second << '\n';
+    std::cout << "----------------------------\n";
   }
 
   return 0;
