b7aaf564b600912012e910cd569ea2918cdf3e5b
JatoMixo <jatomixo@outlook.es>
2022-12-30 12:00:28 +0100

    main

--- LowLevel/cin.cpp
+++ LowLevel/cin.cpp
@@ -4,13 +4,20 @@ using std::string, std::cin, std::cout;
 
 int main(){
 
-  int something = 0;
+  /*int something = 0;
   int s = 0;
   do {
     something += s;
   } while (cin >> s && s != 0);
 
-  cout << something;
+  cout << something;*/
+
+  int nota;
+  string alumno;
+
+  while (cin >> alumno >> nota && alumno != "q" && nota != -1) {
+    cout << "El gilipollas de " << alumno << " ha sacado un " << nota << '\n';
+  }
 
   return 0;
 }
